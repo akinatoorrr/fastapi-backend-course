99a4b86ccc29fded62b23e65613b8403b19a8b4f
Andrey Akinin <akininandrej007@gmail.com>
2025-05-12 04:54:23 +0300

    Add func-based logic to task_tracker

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+task_list = []
+
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return task_list
+
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task: dict = Body(...)):
+    new_id = len(task_list) + 1
+    task_list.append((new_id, task["title"], task.get("status", "New")))
+    return {"message": "Таск создан"}
+
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, data: dict = Body(...)):
+    if 0 < task_id <= len(task_list):
+        task_list[task_id - 1] = (
+            task_id,
+            data["title"],
+            data["status"]
+        )
+        return {"message": "Таск обновлен"}
+    return {"error": "Таск с таким id не найден"}
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    if 0 < task_id <= len(task_list):
+        task_list.pop(task_id - 1)
+        return {"message": "Таск удален"}
+    return {"error": "Таск с таким id не найден"}
