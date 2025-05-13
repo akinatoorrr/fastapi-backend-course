c47cbd82e64effc5ffff1df48c2236256ffe9123
Andrey Akinin <akininandrej007@gmail.com>
2025-05-13 07:58:46 +0300

    Make task_tracker stateless through GitHub Gist

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,24 +1,46 @@
 import json
+import os
+import requests
 from fastapi import FastAPI, Body
+from dotenv import load_dotenv
+
 
 app = FastAPI()
 
+load_dotenv()
+
 
 class TaskFileManager:
+    github_api = (
+        "https://api.github.com/gists/"
+        "fedd8d87e748082ba834bd1db2829c8d"
+    )
+
+    gist_token = os.getenv("GIST_TOKEN")
     file_name = 'tasks.json'
 
     def load_tasks(self):
+        headers = {"Authorization": f"token {self.gist_token}"}
+        resp = requests.get(self.github_api, headers=headers)
+        resp.raise_for_status()
+        gist = resp.json()
+        content = gist["files"][self.file_name]["content"]
         try:
-            with open(self.file_name, 'r', encoding='utf-8') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return []
+            return json.loads(content)
         except json.JSONDecodeError:
             return []
 
     def save_tasks(self, tasks):
-        with open(self.file_name, 'w', encoding='utf-8') as file:
-            json.dump(tasks, file, indent=4, ensure_ascii=False)
+        payload = {
+            "files": {
+                self.file_name: {
+                    "content": json.dumps(tasks, indent=4, ensure_ascii=False)
+                }
+            }
+        }
+        headers = {"Authorization": f"token {self.gist_token}"}
+        resp = requests.patch(self.github_api, json=payload, headers=headers)
+        resp.raise_for_status()
 
 
 file_class = TaskFileManager()
@@ -26,11 +48,13 @@ file_class = TaskFileManager()
 
 @app.get('/tasks')
 def get_tasks():
+    '''Возвращает все имеющиеся задачи в виде списка словарей'''
     return file_class.load_tasks()
 
 
 @app.post('/tasks')
 def create_task(task: dict = Body(...)):
+    '''Создаёт новую задачу, статус по умолчанию - New'''
     tasks = file_class.load_tasks()
     new_id = max((task['id'] for task in tasks), default=0) + 1
     new_task = {
@@ -45,6 +69,7 @@ def create_task(task: dict = Body(...)):
 
 @app.put('/tasks/{task_id}')
 def update_task(task_id: int, data: dict = Body(...)):
+    '''Обновляет название задачи и её статус по указанному в url id'''
     tasks = file_class.load_tasks()
     updated_task = {
         'id': task_id,
@@ -61,6 +86,7 @@ def update_task(task_id: int, data: dict = Body(...)):
 
 @app.delete('/tasks/{task_id}')
 def delete_task(task_id: int):
+    '''Удаляет задачу по указанному в url id'''
     tasks = file_class.load_tasks()
     for i, task in enumerate(tasks):
         if task['id'] == task_id:
