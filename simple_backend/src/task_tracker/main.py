499a48a4291955c4b7f00ed9eecc8ea6f34d1536
Andrey Akinin <akininandrej007@gmail.com>
2025-05-13 12:12:24 +0300

    Add LLM assist to created and updated tasks

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -10,21 +10,42 @@ app = FastAPI()
 load_dotenv()
 
 
+class LLMManager:
+    '''Класс для решения задач с помощью LLM-модели'''
+    ai_token = os.getenv('AI_API_TOKEN')
+    API_BASE_URL = (
+        'https://api.cloudflare.com/client/v4/accounts/'
+        '20899bfe61320c4b5b2765c3cb8ed8be/ai/run/'
+    )
+    headers = {'Authorization': f'Bearer {ai_token}'}
+    model = '@cf/meta/llama-3-8b-instruct'
+
+    def run_model(self, task):
+        payload = {'prompt': f'How to solve this task: {task}'}
+        response = requests.post(
+            f'{self.API_BASE_URL}{self.model}',
+            headers=self.headers,
+            json=payload
+        )
+        return response.json()['result']['response']
+
+
 class TaskFileManager:
+    '''Класс для stateless бэкэнда - храним данные на Github Gist'''
     github_api = (
-        "https://api.github.com/gists/"
-        "fedd8d87e748082ba834bd1db2829c8d"
+        'https://api.github.com/gists/'
+        'fedd8d87e748082ba834bd1db2829c8d'
     )
 
-    gist_token = os.getenv("GIST_TOKEN")
+    gist_token = os.getenv('GIST_TOKEN')
     file_name = 'tasks.json'
 
     def load_tasks(self):
-        headers = {"Authorization": f"token {self.gist_token}"}
-        resp = requests.get(self.github_api, headers=headers)
-        resp.raise_for_status()
-        gist = resp.json()
-        content = gist["files"][self.file_name]["content"]
+        headers = {'Authorization': f'token {self.gist_token}'}
+        response = requests.get(self.github_api, headers=headers)
+        response.raise_for_status()
+        gist = response.json()
+        content = gist['files'][self.file_name]['content']
         try:
             return json.loads(content)
         except json.JSONDecodeError:
@@ -32,18 +53,23 @@ class TaskFileManager:
 
     def save_tasks(self, tasks):
         payload = {
-            "files": {
+            'files': {
                 self.file_name: {
-                    "content": json.dumps(tasks, indent=4, ensure_ascii=False)
+                    'content': json.dumps(tasks, indent=4, ensure_ascii=False)
                 }
             }
         }
-        headers = {"Authorization": f"token {self.gist_token}"}
-        resp = requests.patch(self.github_api, json=payload, headers=headers)
-        resp.raise_for_status()
+        headers = {'Authorization': f'token {self.gist_token}'}
+        response = requests.patch(
+            self.github_api,
+            json=payload,
+            headers=headers
+        )
+        response.raise_for_status()
 
 
 file_class = TaskFileManager()
+ai_class = LLMManager()
 
 
 @app.get('/tasks')
@@ -57,9 +83,10 @@ def create_task(task: dict = Body(...)):
     '''Создаёт новую задачу, статус по умолчанию - New'''
     tasks = file_class.load_tasks()
     new_id = max((task['id'] for task in tasks), default=0) + 1
+    solution = ai_class.run_model(task['title'])
     new_task = {
         'id': new_id,
-        'title': task['title'],
+        'title': task['title'] + ' Solution: ' + solution,
         'status': task.get('status', 'New')
     }
     tasks.append(new_task)
@@ -71,9 +98,10 @@ def create_task(task: dict = Body(...)):
 def update_task(task_id: int, data: dict = Body(...)):
     '''Обновляет название задачи и её статус по указанному в url id'''
     tasks = file_class.load_tasks()
+    solution = ai_class.run_model(data['title'])
     updated_task = {
         'id': task_id,
-        'title': data['title'],
+        'title': data['title'] + ' Solution: ' + solution,
         'status': data['status']
     }
     for i, task in enumerate(tasks):
